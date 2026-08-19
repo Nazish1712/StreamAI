80ed07bd2261ea0254a25433ccc64703c9b87c84
Nazish1712 <nazish1712@gmail.com>
2026-08-19 23:39:09 +0530

    added logo and input field in navbar

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { IconMenu2 } from '@tabler/icons-react'
+import { IconMenu2 , IconBrandYoutube} from '@tabler/icons-react'
 
 const Head = () => {
   return (
     <div>
       <IconMenu2/>
+      <div>
+      <IconBrandYoutube/>
+      <div>StreamAI</div>
+      </div>
+      <div>
+        <input></input>
+      </div>
+      
+      
     </div>
   )
 }
