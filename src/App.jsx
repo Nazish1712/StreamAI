2716dfe64d94416c87a6041a53e562682889fe41
Nazish1712 <nazish1712@gmail.com>
2026-08-25 17:32:09 +0530

    created store and slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
+import { Provider } from "react-redux"
 import Body from "./components/Body"
 import Head from "./components/Head"
-import Sidebar from "./components/Sidebar"
+import store from "./utils/store"
 
 function App() {
-  
-
   return (
+    <Provider store={store}>
     <div className="py-2 px-2">
     <Head/>
     <Body/>
     </div>
-   
+    </Provider>
   )
 }
 
--- src/components/SlideSidebar.jsx
+++ src/components/SlideSidebar.jsx
@@ -62,19 +62,19 @@ const SlideSidebar = () => {
   
 
   return (
-    <div className="h-dvh w-60 bg-neutral-50 shadow-sm">
+    <div className="h-dvh w-60 bg-neutral-50 shadow-sm z-50">
         <div className="ml-[6px] md:ml-[22px] mt-6 flex flex-col gap-6 mr-[6px] md:mr-[22px]">
            {sidebarLinks.map((section)=>(
              <div key={section.title}>
                 <div className="flex gap-1.5 md:gap-2 items-center "> 
                   <div>{section.icon}</div>
-                  <h2 className="font-lato text-base">{section.title}</h2>
+                  <h2 className="font-lato text-base font-bold">{section.title}</h2>
                 </div>
                 
 
                 <div className="flex flex-col gap-1 mt-3 pl-1 md:pl-0">
                {section.content.map((item)=>(
-              <a key={item.heading} className="flex items-center justify-start gap-[9px] md:gap-3">
+              <a key={item.heading} className="flex items-center justify-start gap-[9px] md:gap-3.5">
                     <div>{item.icon}</div>
                     <div className="font-lato text-sm">{item.heading}</div>
                   </a>
