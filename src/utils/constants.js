4c532c61482b2763708a9af4db621e64fdf9f3a0
Nazish1712 <nazish1712@gmail.com>
2026-08-26 14:38:06 +0530

    Secured API key using environment variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 
-const GOOGLE_API_KEY = "AIzaSyAFx-iWyXQKajwlCwzjcPfayNA0W_30joc"
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" + GOOGLE_API_KEY;
 
