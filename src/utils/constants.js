a96203132d79056176418566e9049f44e19d0365
Nazish1712 <nazish1712@gmail.com>
2026-08-26 14:28:09 +0530

    added youtube api

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+
+const GOOGLE_API_KEY = "AIzaSyAFx-iWyXQKajwlCwzjcPfayNA0W_30joc"
+
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" + GOOGLE_API_KEY;
+
