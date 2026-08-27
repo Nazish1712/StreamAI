ea309440425232ab60185eaad7db2457a4c872d5
Nazish1712 <nazish1712@gmail.com>
2026-08-27 17:46:29 +0530

    added shimmer component

---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,35 @@
+const Shimmer = () => {
+    return (
+     
+          <div className="group bg-white/5 backdrop-blur-sm border border-white/10 rounded-xl overflow-hidden animate-pulse">
+      
+            <div className="h-40 w-full bg-white/10"></div>
+      
+            <div className="p-4 space-y-3">
+      
+              <div className="flex justify-between items-center">
+      
+                <div className="h-5 w-32 bg-white/10 rounded"></div>
+      
+                <div className="h-5 w-20 bg-white/10 rounded"></div>
+      
+              </div>
+      
+              <div className="flex gap-2 items-center">
+      
+                <div className="h-4 w-4 bg-white/10 rounded-full"></div>
+      
+                <div className="h-4 w-40 bg-white/10 rounded"></div>
+      
+              </div>
+      
+              <div className="h-px w-full bg-white/10"></div>
+      
+              <div className="h-10 w-full bg-white/10 rounded-lg"></div>
+      
+            </div>
+      
+          </div>
+        
+    )}
+  export default Shimmer
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIDEOS_API } from '../utils/constants'
 import VideoCard from './VideoCard'
+import Shimmer from './Shimmer'
 
 const VideoContainer = () => {
 
@@ -18,13 +19,23 @@ const getVideos = async () => {
 }
 
 
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-2 gap-4">
+return(
+  videos.length === 0 ?  (
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-45 px-6">
+  {Array(10).fill("").map((_, index) => (
+     <Shimmer key={index}/>
+  ))}
+</div>
+) :(
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-2 gap-4">
       {videos.map((video)=>(
         <VideoCard key={video.id} info={video}/>
       ))}
       
     </div>
+)
+
+    
   )
 }
 
